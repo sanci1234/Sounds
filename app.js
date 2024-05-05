f89ace54045ddccb8a61d70380b9cf60ba442829
Ibrahim Şancı <ibrahimsanci613@gmail.com>
2024-05-05 18:32:16 +0300

    Sounds

---
+++ app.js
@@ -0,0 +1,20 @@
+const sounds = ["piano","strange","gospel","underwater","scifi","production"];
+sounds.forEach((sound)=>{
+    const btn = document.createElement("button");
+    btn.classList.add("btn")
+    btn.innerText = sound;
+
+    btn.addEventListener("click",()=>{
+        stopSongs();
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById("buttons").appendChild(btn);
+})
+function stopSongs(){
+    sounds.forEach((sound)=>{
+        const song = document.getElementById(sound);
+        song.pause();
+        song.currentTime = 0;
+    })
+}
